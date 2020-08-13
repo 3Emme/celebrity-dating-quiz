a1b952c15a58354de415d8ee98efd538768f8c16
CameronWill <cellardoorsolar@aol.com>
2020-08-13 14:48:17 -0700

    add functions and events, photos, selectors, styles
    Co-Authored-By: Emme Buentiempo <emmettbuentiempo@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,31 @@
 $(document).ready(function() {
+
+  $("select#interest").submit(function(event) {
+    const interest = $("select#interest").val();
+
+    if (interest = "love"){
+      $("#love-scale").show();
+      } else {
+      $("#hate-scale").show();
+      }
+
+    event.preventDefault();
+  });
+
   $("form#horse").submit(function(event) {
     const love = parseInt($("input#love").val());
     const hate = parseInt($("input#hate").val());
     const interest = $("select#interest").val();
 
-    if (love) {
-      let quote = (100 - love) * 3;
-      if (interest === 'love' && love > 5) {
-        quote += 50;
+      if (interest === "love" && love > 5){
+      $("#foxx").show();
+      } else if (interest ==="hate" && hate > 5){
+      $("#stewart").show();
+      } else {
+      $("#busey").show();
       }
 
-      //if (interest === "love" && love > 5){
-      //$("#date1").show();
-      //} else if (interest ==="hate" && hate > 5){
-      //$("date3").show();
-      //} else {
-      //$()
-      //
-      $("#rate").text(quote);
-      $("#quote").show();
-    } else {
+      if (!interest || !love || !hate){
       alert('Please enter how strongly you feel about your love/hate.');
     }
 
