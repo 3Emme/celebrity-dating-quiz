f8a8befdecbc6cecd57d2bbd667dfdd6ce929af3
CameronWill <cellardoorsolar@aol.com>
2020-08-13 14:06:36 -0700

    add formers, selectors, button, script functions
    Co-Authored-By: Emme Buentiempo <emmettbuentiempo@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,27 @@
 $(document).ready(function() {
-  $("form#insurance").submit(function(event) {
-    const age = parseInt($("input#age").val());
-    const gender = $("select#gender").val();
+  $("form#horse").submit(function(event) {
+    const love = parseInt($("input#love").val());
+    const hate = parseInt($("input#hate").val());
+    const interest = $("select#interest").val();
 
-    const quote = (100 - age) * 3;
+    if (love) {
+      let quote = (100 - love) * 3;
+      if (interest === 'love' && love > 5) {
+        quote += 50;
+      }
 
-    $("#rate").text(quote);
-    $("#quote").show();
+      //if (interest === "love" && love > 5){
+      //$("#date1").show();
+      //} else if (interest ==="hate" && hate > 5){
+      //$("date3").show();
+      //} else {
+      //$()
+      //
+      $("#rate").text(quote);
+      $("#quote").show();
+    } else {
+      alert('Please enter how strongly you feel about your love/hate.');
+    }
 
     event.preventDefault();
   });
